b3a8bd6bb833ab7044008343be9ad0fd3b75ed66
NoahDammer <noah@thinksoft.ca>
2025-06-24 11:44:44 -0400

    feat: :tada: thinksoft_labels initial commit
    This is the new thinksoft_labels module that will be used for the new Quest Odoo 18

---
+++ thinksoft_labels/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Thinksoft Labels",
+    "description": """
+        All labels live here.
+    """,
+    "author": "Thinksoft Inc.",
+    "website": "http://www.thinksoft.ca",
+    "license": "LGPL-3",
+    "category": "Addons Custom/Thinksoft Labels",
+    "version": "0.1",
+    "depends": ["base"],
+    "data": [
+        # "reports/ir_actions_report.xml",
+    ],
+}
