b82e0e739cf413a5099404246227ba75a3ea1fd1
NoahDammer <noah@thinksoft.ca>
2025-06-24 11:42:46 -0400

    feat: :tada: thinksoft_reports initial commit
    This is the new thinksoft_reports module that will be used for the new Quest Odoo 18

---
+++ thinksoft_reports/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Thinksoft Reports",
+    "description": """
+        All reports live here.
+    """,
+    "author": "Thinksoft Inc.",
+    "website": "http://www.thinksoft.ca",
+    "license": "LGPL-3",
+    "category": "Addons Custom/Thinksoft Reports",
+    "version": "0.1",
+    "depends": ["base"],
+    "data": [
+        # "reports/ir_actions_report.xml",
+    ],
+}
