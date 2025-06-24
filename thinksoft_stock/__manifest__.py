abdc1cc7e19bb2357ac87c7cddc3b20c6ae3e556
NoahDammer <noah@thinksoft.ca>
2025-06-24 11:09:05 -0400

    feat: :tada: thinksoft_stock initial commit
    This is the new thinksoft_stock module that will be used for the new Quest Odoo 18

---
+++ thinksoft_stock/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Stock Extension",
+    "description": """
+        This module adds features to Stock module which is for the delivery orders and transfers side of things.
+    """,
+    "author": "Thinksoft Inc.",
+    "website": "http://www.thinksoft.ca",
+    "license": "LGPL-3",
+    "category": "Addons Custom/Thinksoft Stock",
+    "version": "0.1",
+    "depends": ["base", "stock"],
+    "data": [
+        # "stock_picking_views.xml",
+    ],
+}
