0ea2569bcdde7fe0f8ee54664c7e71fc56b583e7
NoahDammer <noah@thinksoft.ca>
2025-06-30 15:33:39 -0400

    feat: :tada: thinksoft_mtr initial commit
    #6

---
+++ thinksoft_mtr/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "MTR",
+    "description": """
+        Metal Treatment Report (MTR) module for Odoo.
+    """,
+    "author": "Thinksoft Inc.",
+    "website": "http://www.thinksoft.ca",
+    "license": "LGPL-3",
+    "category": "Addons Custom/Thinksoft MTR",
+    "application": True,
+    "version": "0.1",
+    "depends": ["base"],
+    "data": [],
+}
