bae6abbc82a1b28d2798da8646773770947ee96c
NoahDammer <noah@thinksoft.ca>
2025-06-30 15:38:15 -0400

    feat: :tada: thinksoft_ncr initial commit
    #7

---
+++ thinksoft_ncr/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "NCR",
+    "description": """
+        Non-Conformance Report (NCR) module for Odoo.
+    """,
+    "author": "Thinksoft Inc.",
+    "website": "http://www.thinksoft.ca",
+    "license": "LGPL-3",
+    "category": "Addons Custom/Thinksoft NCR",
+    "application": True,
+    "version": "0.1",
+    "depends": ["base"],
+    "data": [],
+}
