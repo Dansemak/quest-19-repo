33e3ac416c2198014d93449e4d139a9119c26122
NoahDammer <noah@thinksoft.ca>
2025-07-02 12:33:50 -0400

    feat: :sparkles: Add state, classification and form view
    #7

--- thinksoft_ncr/models/ncr_claim.py
+++ thinksoft_ncr/models/ncr_claim.py
@@ -1,9 +1,28 @@
-from odoo import fields, models, _
+from odoo import _, fields, models
 
 
 class NcrClaim(models.Model):
-    _name = 'ncr.claim'
-    _description = 'Non-Conformance Report'
+    _name = "ncr.claim"
+    _description = "Non-Conformance Report"
 
-    name = fields.Char(string='Name', required=True, copy=False, readonly=True, index=True,
-                       default=lambda self: _('New'))
+    state = fields.Selection(
+        [("open", "Open"), ("closed", "Closed"), ("cancelled", "Cancelled")],
+        string="State",
+        default="open",
+        tracking=True,
+        copy=False,
+        readonly=False,
+    )
+    name = fields.Char(
+        string="Name",
+        required=True,
+        copy=False,
+        readonly=True,
+        index=True,
+        default=lambda self: _("New"),
+    )
+    classification = fields.Selection(
+        [("nc", "Non-Conformance"), ("find", "Finding")],
+        string="Classification",
+        required=True,
+    )
