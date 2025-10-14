d3f13ec70891437243cd561a6cba849f59109ed4
bigoa-gach <bigoagach@gmail.com>
2025-10-14 09:42:38 -0600

    feat: :sparkles: added seq_no (line number) to sale.order.line model

--- thinksoft_sale_ext/models/sale_order_line.py
+++ thinksoft_sale_ext/models/sale_order_line.py
@@ -3,5 +3,23 @@ from odoo import api, fields, models
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
+    _order = 'seq_no, id'
 
+    seq_no = fields.Integer(string="#", compute="_compute_line_number", readonly=True, store=True)
     tagging = fields.Char(help="Customer Line Item Reference for custom identification or referencing of product in accordance to the customer")
+
+    # determining the line number of the sale.order.line record
+    @api.depends('order_id', "order_id.order_line", "sequence")
+    def _compute_line_number(self):
+        for line in self:
+            if line.order_id:
+                # getting all of the lines in the order
+                lines = line.order_id.order_line.sorted(key=lambda l: (l.sequence, l.id))
+
+                # finding the position of this line and assigning its line number
+                for i, o_l, in enumerate(lines, start=1):
+                    if o_l == line:
+                        line.seq_no = i * 10
+                        break
+            else:
+                line.seq_no = 0
