b814e6b3ad506484cd4820d2feede8e09818f5a9
bigoa-gach <bigoagach@gmail.com>
2025-10-15 14:58:26 -0600

    fix: :pencil2: fixed typo

--- thinksoft_sale_ext/models/crm_lead.py
+++ thinksoft_sale_ext/models/crm_lead.py
@@ -7,13 +7,13 @@ class CrmLead(models.Model):
     # adding relational fields to crm.lead in the thinksoft_sale_ext module
     # to avoid hard dependancies between extension modules
 
-    company_end_user_id = fields.Many2one('company.user', string="End User")
+    company_user_id = fields.Many2one('company.user', string="End User")
     job_project_id = fields.Many2one('job.project', string="Job Project")
 
     def action_sale_quotations_new(self):
         res = super().action_sale_quotations_new()
 
-        res['context']['default_company_end_user_id'] = self.company_end_user_id.id
+        res['context']['default_company_user_id'] = self.company_user_id.id
         res['context']['default_job_project_id'] = self.job_project_id.id
 
         return res
