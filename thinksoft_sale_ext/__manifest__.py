2638673ca7a92a93d5961037be46f29636d98e16
NoahDammer <noah@thinksoft.ca>
2025-10-27 13:40:28 -0400

    refactor: :truck: Rename `sales_extended_security.xml` to `sale_security_groups.xml`
    This gives a more clear description that this file contains security groups

--- thinksoft_sale_ext/__manifest__.py
+++ thinksoft_sale_ext/__manifest__.py
@@ -11,7 +11,7 @@
     "version": "19.0.0.0.1",
     "depends": ["base", "sale", "delivery", "stock", 'sale_stock', 'crm',],
     "data": [
-        'security/sales_extended_security.xml',
+        'security/sale_security_groups.xml',
         'security/ir.model.access.csv',
         "views/res_partner_views.xml",
         "views/sale_order_views.xml",
