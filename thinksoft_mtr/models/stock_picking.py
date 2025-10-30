88c71bc08c18de6cc00000ae516cb0a3ca9b51e0
NoahDammer <noah@thinksoft.ca>
2025-10-30 18:23:21 -0400

    refactor: :fire: Temporarily remove MTR
    #6

--- thinksoft_mtr/models/stock_picking.py
+++
@@ -1,9 +0,0 @@
-from odoo import fields, models
-
-
-class StockPicking(models.Model):
-    _inherit = "stock.picking"
-
-    mtr_move_ids = fields.One2many(
-        "stock.move", "picking_id", string="MTR Stock Moves", copy=True
-    )
