820affbf9861449276b8a0bc4b5639d01eca7b2c
NoahDammer <noah@thinksoft.ca>
2025-11-04 11:07:14 -0500

    feat: :tada: thinksoft_no_create first commit
    #67

---
+++ thinksoft_no_create/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Thinksoft No Create",
+    "version": "19.0.0.1",
+    "description": "Thinksoft No Create blocks creation of customers, products, etc... on built-in fields.",
+    "summary": "Thinksoft No Create For Built-in Fields",
+    "author": "Thinksoft Inc.",
+    "license": "AGPL-3",
+    "category": "Addons Custom/Thinksoft",
+    "depends": [],
+    "data": [],
+    "auto_install": False,
+    "application": False,
+}
