2393bf2949115ab0e237eb076f44a4f6bf413d55
bigoa-gach <bigoagach@gmail.com>
2025-11-05 13:55:53 -0700

    feat: :sparkles: added reset to draft button to batch payment headers

---
+++ thinksoft_account_ext/models/account_batch_payment.py
@@ -0,0 +1,10 @@
+from odoo import fields, models, api
+
+class AccountBatchPayment(models.Model):
+    _inherit = "account.batch.payment"
+
+    # resets sent batch payments to draft + unmarks payments that are sent
+    def reset_draft(self):
+        if self.state == "sent":
+            self.write({"state": "draft"})
+            self.payment_ids.unmark_as_sent()
