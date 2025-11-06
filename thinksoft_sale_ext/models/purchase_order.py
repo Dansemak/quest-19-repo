40fe4a601fe8aafeda1a272903cedff50898a9bd
bigoa-gach <bigoagach@gmail.com>
2025-11-06 10:19:57 -0700

    fix: :bug: fixed purchase.order form view name

--- thinksoft_sale_ext/models/purchase_order.py
+++ thinksoft_sale_ext/models/purchase_order.py
@@ -3,6 +3,9 @@ from odoo import api, models, fields
 class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
 
+    # adding relational fields to purchase.order in the thinksoft_sale_ext module
+    # to avoid hard dependancies between extension modules
+
     partner_contact_id = fields.Many2one("res.partner", string="Vendor Contact")
     sale_freight_id = fields.Many2one(
         "sale.freight",
