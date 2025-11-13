89fc0a3dd5b6c2ad6211c0a5e5e91f2d5ec7b112
bigoa-gach <bigoagach@gmail.com>
2025-11-13 11:48:48 -0700

    fix: :bug: fixed (added) waybill computation

--- thinksoft_account_ext/models/account_move.py
+++ thinksoft_account_ext/models/account_move.py
@@ -1,7 +1,30 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class AccountMove(models.Model):
     _inherit = "account.move"
 
-    waybill = fields.Char(string="Waybill")
+    waybill = fields.Char(string="Waybill", compute="_compute_waybill")
+
+    # getting the waybill data from the related sales orders', related stock pickings' waybill
+    @api.depends("invoice_line_ids")
+    def _compute_waybill(self):
+        for move in self:
+            waybills = []
+
+            # getting all related sale orders from invoice lines
+            sale_orders = move.invoice_line_ids.sale_line_ids.order_id
+
+            # looking through all pickings from sales orders
+            for order in sale_orders:
+                # finding all the OUT pickings with waybills
+                outgoing_pickings = order.picking_ids.filtered(
+                    lambda p: p.picking_type_id.sequence_code == "OUT" and p.waybill
+                )
+
+                # getting all the waybills
+                for picking in outgoing_pickings:
+                    if picking.waybill and picking.waybill not in waybills:
+                        waybills.append(picking.waybill)
+
+            move.waybill = ", ".join(waybills) if waybills else False
