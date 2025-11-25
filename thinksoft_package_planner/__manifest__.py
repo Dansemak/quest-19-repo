8450ee841a6e1aa599874dec1276b9c8eee8fd25
NoahDammer <noah@thinksoft.ca>
2025-11-25 18:09:51 -0500

    feat: :tada: Thinksoft_package_planner first commit
    Adds some temp test buttons as a base
    
    #89

---
+++ thinksoft_package_planner/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Package Planner",
+    "description": """
+        Package planner module for Odoo.
+    """,
+    "author": "Thinksoft Inc.",
+    "website": "http://www.thinksoft.ca",
+    "license": "LGPL-3",
+    "category": "Addons Custom/Thinksoft Package Planner",
+    "application": True,
+    "version": "19.0.0.1",
+    "depends": ["base", "stock"],
+    "data": [
+        "views/stock_picking_views.xml",
+    ],
+}
