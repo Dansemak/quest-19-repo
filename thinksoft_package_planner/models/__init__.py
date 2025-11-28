11592100ec0a08ed021f45e46edd6b4d5a0b18c1
NoahDammer <noah@thinksoft.ca>
2025-11-28 11:53:09 -0500

    refactor: :truck: Move label py files
    #89

--- thinksoft_package_planner/models/__init__.py
+++ thinksoft_package_planner/models/__init__.py
@@ -1,6 +1,4 @@
 from . import package_planner_line
 from . import package_line
 from . import stock_picking
-from . import box_label
-from . import box_label_skid
 from . import stock_move
---
+++ thinksoft_package_planner/report/__init__.py
@@ -0,0 +1,2 @@
+from . import box_label
+from . import box_label_skid
