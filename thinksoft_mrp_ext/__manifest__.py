3e86d2a95a512820d055834a0d3ec462d82ca222
NoahDammer <noah@thinksoft.ca>
2025-12-02 00:28:31 -0500

    feat: :tada: thinksoft_mrp_ext first commit
    #96

---
+++ thinksoft_mrp_ext/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "Thinksoft Manufacturing Extension",
+    "summary": "Thinksoft Manufacturing Extension",
+    "description": """
+        This module adds features to the Manufacturing module.
+    """,
+    "author": "Thinksoft Inc.",
+    "website": "http://www.thinksoft.ca",
+    "license": "AGPL-3",
+    "category": "Addons Custom/Thinksoft",
+    "version": "19.0.0.0.1",
+    "depends": ["base"],
+    "data": [
+        "views/mrp_production_views.xml"
+    ],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+}
---
+++ thinksoft_mrp_ext/models/mrp_production.py
@@ -0,0 +1,5 @@
+from odoo import models, fields, api
+
+
+class MrpProduction(models.Model):
+    _inherit = 'mrp.production'
