2e861fd264e5e15209ffff3359a3e32875e6e78f
bigoa-gach <bigoagach@gmail.com>
2025-12-04 14:17:41 -0700

    refactor: :lipstick: hide base Odoo print menus on sale.order

--- thinksoft_actions/__manifest__.py
+++ thinksoft_actions/__manifest__.py
@@ -2,16 +2,26 @@
     "name": "Thinksoft Actions",
     "summary": "All actions live here.",
     "description": """
-        All actions (specifically for labels) live here.
+        All actions (specifically for labels and reports) live here.
     """,
     "author": "Thinksoft Inc.",
     "website": "http://www.thinksoft.ca",
     "license": "AGPL-3",
     "category": "Addons Custom/Thinksoft",
     "version": "19.0.0.0.1",
-    "depends": ["base", "thinksoft_labels", "stock"],
+    "depends": [
+        "base",
+        "thinksoft_labels",
+        "thinksoft_reports",
+        "stock",
+        "sale",
+        "sale_timesheet",
+        "sale_pdf_quote_builder",
+    ],
     "data": [
         "views/stock_picking_views.xml",
+        "views/sale_order_views.xml",
+        "report/ir_actions_report.xml",
     ],
     "installable": True,
     "application": False,
