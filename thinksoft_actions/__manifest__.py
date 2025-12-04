1343040bd3859535b000e7ecc42246b74ad01c01
bigoa-gach <bigoagach@gmail.com>
2025-12-04 10:18:09 -0700

    feat: :heavy_plus_sign: created thinksoft_actions for actions; added 4x4 report

---
+++ thinksoft_actions/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "Thinksoft Actions",
+    "summary": "All actions live here.",
+    "description": """
+        All actions (specifically for labels) live here.
+    """,
+    "author": "Thinksoft Inc.",
+    "website": "http://www.thinksoft.ca",
+    "license": "AGPL-3",
+    "category": "Addons Custom/Thinksoft",
+    "version": "19.0.0.0.1",
+    "depends": ["base", "thinksoft_labels", "stock"],
+    "data": [
+        "views/stock_picking_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+}
--- thinksoft_labels/__manifest__.py
+++ thinksoft_labels/__manifest__.py
@@ -17,6 +17,7 @@
         "reports/shipping_label_4x4_templates.xml",
         "reports/location_labels.xml",
         "reports/4x4_label_for_all_templates.xml",
+        "reports/4x4_label_templates.xml",
         # Actions and menus always last
         "reports/ir_actions_report.xml",
     ],
