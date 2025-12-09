4a05238a6a6009e7bc2024211fab140d0493f497
NoahDammer <noah@thinksoft.ca>
2025-12-09 10:30:44 -0500

    refactor: :memo: Update manifest
    #89

--- thinksoft_package_planner/__manifest__.py
+++ thinksoft_package_planner/__manifest__.py
@@ -1,25 +1,21 @@
 {
     'name': 'Package Planner',
-    'version': '1.3',
+    'version': '19.0.0.1',
     'category': 'Addons Custom/Thinksoft Package Planner',
-    'description': """ Order Fullfillment Packaging Tool July 2021
-
-    The purpose of this module is to generate the labels needed for a shipment when an order
-    is fullfilled.  By using this planner the user will be able to print labels for each
-    box and pallet required in an order.  The labels will list the product and 
-    quantities that are in the box or pallet.  If more than one box is used in the
-    shipment, the label will indicate a box number of quantity value such as BOX 1 of 3.
-    The same is true for pallets.  The labels will dislay the order number,
-    shipping address and quantities.  This makes it easier for receivers to quickly 
-    locate a specific product when receiving a shipment.
-
-
+    'description': """
+        The purpose of this module is to generate the labels needed for a shipment when an order
+        is fullfilled.  By using this planner the user will be able to print labels for each
+        box and pallet required in an order.  The labels will list the product and 
+        quantities that are in the box or pallet.  If more than one box is used in the
+        shipment, the label will indicate a box number of quantity value such as BOX 1 of 3.
+        The same is true for pallets.  The labels will dislay the order number,
+        shipping address and quantities.  This makes it easier for receivers to quickly 
+        locate a specific product when receiving a shipment.
     """,
     'author': 'Thinksoft Inc',
     'website': 'http://www.thinksoft.ca',
     'license': 'LGPL-3',
     'depends': ['stock','thinksoft_mtr'],
-    'init_xml': [],
     'data': [
         'security/ir.model.access.csv',
         'views/stock_picking_views.xml',
@@ -27,10 +23,5 @@
         'report/box_label_template.xml',
         'report/skid_label_template.xml',
     ],
-    'demo_xml': [],
-    'test': [],
-    'installable': True,
-    'active': False,
-
+    "application": True,
 }
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
