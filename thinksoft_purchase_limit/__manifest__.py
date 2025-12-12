60168eb8d4ce45f82e473e08265960312bafaa40
NoahDammer <noah@thinksoft.ca>
2025-12-12 10:44:22 -0500

    feat: :tada: Purchase Order Limit First Commit
    #103

---
+++ thinksoft_purchase_limit/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Purchase Order Limit",
+    "description": """
+        This adds a dolar amount limit to each user to limit the amount of a purchase order they can make.
+    """,
+    "author": "Thinksoft Inc.",
+    "website": "http://www.thinksoft.ca",
+    "license": "LGPL-3",
+    "category": "Addons Custom/Thinksoft Purchase Limit",
+    "version": "19.0.0.1",
+    "depends": ["base"],
+    "data": [
+        "views/res_users_views.xml",
+    ],
+}
