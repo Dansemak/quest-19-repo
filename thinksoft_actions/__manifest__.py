1e3d3d6d1c85c3641a07acbc9d4d06e13ad7882e
bigoa-gach <bigoagach@gmail.com>
2025-12-15 15:35:53 -0700

    refactor: :bug: fixed purchase order report print

--- thinksoft_actions/__manifest__.py
+++ thinksoft_actions/__manifest__.py
@@ -27,6 +27,7 @@
         "views/stock_picking_views.xml",
         "views/sale_order_views.xml",
         "views/account_move_views.xml",
+        "views/purchase_order_views.xml",
         "report/ir_actions_report.xml",
     ],
     "installable": True,
