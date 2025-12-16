1a012b4d19e9e20f556eb9a35fcb4ea6e15ad91c
NoahDammer <noah@thinksoft.ca>
2025-12-16 17:05:45 -0500

    refactor: :recycle: User current user limit instead of buyer
    #103

--- thinksoft_purchase_limit/models/purchase_order.py
+++ thinksoft_purchase_limit/models/purchase_order.py
@@ -4,15 +4,15 @@ from odoo import fields, models
 class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
 
-    purchase_limit = fields.Monetary(related="user_id.purchase_limit")
     purchase_hold = fields.Boolean(string="On Hold", default=False, copy=False)
 
     def button_confirm(self):
         for order in self:
-            if order.amount_total > order.purchase_limit:
-                order.message_post(body="\u2022 Purchase Hold")
+            if order.amount_total > self.env.user.purchase_limit:
                 order.write({"purchase_hold": True})
+                order.message_post(body="Purchase Hold}")
                 return
-            if order.amount_total <= order.purchase_limit:
+            if order.amount_total <= self.env.user.purchase_limit:
                 order.write({"purchase_hold": False})
+                order.message_post(body=f"Purchase Order Confirmed By: {self.env.user.name}")
         return super().button_confirm()
