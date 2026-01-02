62ea355ae2f4bb12b9b65b359ae0df73c53d0391
NoahDammer <noah@thinksoft.ca>
2026-01-01 23:48:19 -0500

    feat: :lock: Add no_create to Stock and Purchase
    #67

--- thinksoft_no_create/__manifest__.py
+++ thinksoft_no_create/__manifest__.py
@@ -1,16 +1,18 @@
 {
     "name": "Thinksoft No Create",
-    "version": "19.0.1.0",
+    "version": "19.0.1.1",
     "description": "Thinksoft No Create blocks creation of customers, products, etc... on built-in fields.",
     "summary": "Thinksoft No Create For Built-in Fields",
     "author": "Thinksoft Inc.",
     "license": "AGPL-3",
     "category": "Addons Custom/Thinksoft",
-    "depends": ["sale", "account"],
+    "depends": ["sale", "account", "stock", "purchase"],
     "data": [
-        "views/sale_order_views.xml",
         "views/account_move_views.xml",
+        "views/purchase_order_views.xml",
+        "views/sale_order_views.xml",
+        "views/stock_picking_views.xml",
     ],
-    "auto_install": False,
+    "auto_install": False,``
     "application": False,
 }
