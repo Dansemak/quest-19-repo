08aed7b07b4c3eeb633621cb512bf2b34c56c9d4
NoahDammer <noah@thinksoft.ca>
2026-01-05 21:50:44 -0500

    refactor: :bookmark: Version 19.0.1.6
    #33

--- thinksoft_reports/__manifest__.py
+++ thinksoft_reports/__manifest__.py
@@ -8,7 +8,7 @@
     "website": "http://www.thinksoft.ca",
     "license": "AGPL-3",
     "category": "Addons Custom/Thinksoft",
-    "version": "19.0.1.5",
+    "version": "19.0.1.6",
     "depends": ["base"],
     "data": [
         "reports/reports_paperformats.xml",
