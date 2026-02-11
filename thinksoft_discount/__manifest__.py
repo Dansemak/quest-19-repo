e247ec73f914d8d47ca3f00a81c02cdc89f202c2
NoahDammer <noah@thinksoft.ca>
2026-02-10 22:58:44 -0500

    feat: :tada: thinksoft_discount
    #311

---
+++ thinksoft_discount/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Thinksoft Discount",
+    "summary": "Thinksoft Discount",
+    "description": """
+        This module makes the subtotals on Sale Order Lines use the rounded discount unit price.
+    """,
+    "author": "Thinksoft Inc.",
+    "website": "http://www.thinksoft.ca",
+    "license": "AGPL-3",
+    "category": "Addons Custom/Thinksoft",
+    "version": "19.0.1.0",
+    "depends": ["base", "sale"],
+    "data": [
+        "views/sale_order_views.xml",
+    ],
+}
---
+++ thinksoft_discount/models/sale_order_line.py
@@ -0,0 +1,29 @@
+from odoo import api, fields, models
+
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    price_unit_discounted = fields.Monetary("Unit Price Discount")
+
+    @api.depends('product_uom_qty', 'discount', 'price_unit', 'tax_ids')
+    def _compute_amount(self):
+        for line in self:
+            if line.discount > 0.0:
+                line.price_unit_discounted = round(line.price_unit * (1 - (line.discount / 100.0)), 2)
+            else:
+                line.price_unit_discounted = line.price_unit
+
+        originals = [(line, line.price_unit, line.discount) for line in self]
+        try:
+            for line, _, _ in originals:
+                line.price_unit = line.price_unit_discounted
+                line.discount = 0.0
+
+            res = super(SaleOrderLine, self)._compute_amount()
+        finally:
+            for line, orig_price, orig_discount in originals:
+                line.price_unit = orig_price
+                line.discount = orig_discount
+
+        return res
