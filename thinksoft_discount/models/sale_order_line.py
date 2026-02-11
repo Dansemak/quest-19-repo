74880df13a2c976f6f62132ffabe598fe4a3d0f8
NoahDammer <noah@thinksoft.ca>
2026-02-10 23:28:30 -0500

    refactor: :zap: Use built-in price_reduce_taxexcl instead
    #311

--- thinksoft_discount/models/sale_order_line.py
+++ thinksoft_discount/models/sale_order_line.py
@@ -4,20 +4,20 @@ from odoo import api, fields, models
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    price_unit_discounted = fields.Monetary("Unit Price Discount")
+    price_reduce_taxexcl = fields.Monetary(compute="_compute_amount")
 
     @api.depends('product_uom_qty', 'discount', 'price_unit', 'tax_ids')
     def _compute_amount(self):
         for line in self:
             if line.discount > 0.0:
-                line.price_unit_discounted = round(line.price_unit * (1 - (line.discount / 100.0)), 2)
+                line.price_reduce_taxexcl = round(line.price_unit * (1 - (line.discount / 100.0)), 2)
             else:
-                line.price_unit_discounted = line.price_unit
+                line.price_reduce_taxexcl = line.price_unit
 
         originals = [(line, line.price_unit, line.discount) for line in self]
         try:
             for line, _, _ in originals:
-                line.price_unit = line.price_unit_discounted
+                line.price_unit = line.price_reduce_taxexcl
                 line.discount = 0.0
 
             res = super(SaleOrderLine, self)._compute_amount()
