7f086939f4d78c1ad6c254cf89fff015274eea30
NoahDammer <noah@thinksoft.ca>
2026-02-13 14:59:31 -0500

    refactor: :wastebasket: Clean up code
    #311

--- thinksoft_discount/models/sale_order_line.py
+++ thinksoft_discount/models/sale_order_line.py
@@ -14,16 +14,13 @@ class SaleOrderLine(models.Model):
             else:
                 line.price_reduce_taxexcl = line.price_unit
 
-        originals = [(line, line.price_unit, line.discount) for line in self]
+            original_price_unit = line.price_unit
+            original_discount = line.discount
 
-        for line, _, _ in originals:
             line.price_unit = line.price_reduce_taxexcl
             line.discount = 0.0
 
-        res = super(SaleOrderLine, self)._compute_amount()
+            super(SaleOrderLine, self)._compute_amount()
 
-        for line, orig_price, orig_discount in originals:
-            line.price_unit = orig_price
-            line.discount = orig_discount
-
-        return res
+            line.price_unit = original_price_unit
+            line.discount = original_discount
