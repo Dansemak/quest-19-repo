aba7a6fdc535b5f0559efb30544e77ab8c36cf65
NoahDammer <noah@thinksoft.ca>
2026-02-20 17:06:39 -0500

    docs: :memo: Add notes
    #311

--- thinksoft_discount/models/sale_order_line.py
+++ thinksoft_discount/models/sale_order_line.py
@@ -4,8 +4,18 @@ from odoo import api, fields, models
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
+    # This switches and repurposes the compute field use to _compute_amount
     price_reduce_taxexcl = fields.Monetary(compute="_compute_amount")
 
+    # 2026-02-20
+    # _compute_amount is copied directly from https://github.com/odoo/odoo/blob/19.0/addons/sale/models/sale_order_line.py#L844
+    # (The line the link points to may move over time but _compute_amount is in sale_order_line.py)
+    # 
+    # It sets the price_reduce_taxexcl field and it also hijacks base_line to temporarily
+    # set 'price_unit' to 'price_reduce_taxexcl' and 'discount' to '0.0' so that it can 
+    # do the calculations with the rounded discounted unit price instead.
+    # The only modified parts of _compute_amount is the sections 'Set price_reduce_taxexcl'
+    # and 'Hijack'.
     @api.depends('product_uom_qty', 'discount', 'price_unit', 'tax_ids')
     def _compute_amount(self):
         AccountTax = self.env['account.tax']
