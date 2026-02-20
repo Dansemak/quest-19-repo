5d7cad0eb94a070720cbf376987c60c4751fba1b
NoahDammer <noah@thinksoft.ca>
2026-02-20 16:56:06 -0500

    refactor: :art: Bring over and modify _compute_amount
    #311

--- thinksoft_discount/models/sale_order_line.py
+++ thinksoft_discount/models/sale_order_line.py
@@ -8,19 +8,23 @@ class SaleOrderLine(models.Model):
 
     @api.depends('product_uom_qty', 'discount', 'price_unit', 'tax_ids')
     def _compute_amount(self):
+        AccountTax = self.env['account.tax']
         for line in self:
+            ##################### Set price_reduce_taxexcl #####################
             if line.discount > 0.0:
                 line.price_reduce_taxexcl = round(line.price_unit * (1 - (line.discount / 100.0)), 2)
             else:
                 line.price_reduce_taxexcl = line.price_unit
-
-            original_price_unit = line.price_unit
-            original_discount = line.discount
-
-            line.price_unit = line.price_reduce_taxexcl
-            line.discount = 0.0
-
-            super(SaleOrderLine, self)._compute_amount()
-
-            line.price_unit = original_price_unit
-            line.discount = original_discount
+            ####################################################################
+            
+            company = line.company_id or self.env.company
+            base_line = line._prepare_base_line_for_taxes_computation()
+            ############################## Hijack ##############################
+            base_line['price_unit'] = line.price_reduce_taxexcl
+            base_line['discount'] = 0.0
+            ####################################################################
+            AccountTax._add_tax_details_in_base_line(base_line, company)
+            AccountTax._round_base_lines_tax_details([base_line], company)
+            line.price_subtotal = base_line['tax_details']['total_excluded_currency']
+            line.price_total = base_line['tax_details']['total_included_currency']
+            line.price_tax = line.price_total - line.price_subtotal
