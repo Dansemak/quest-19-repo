965f36305a785c18553ac9d350ff43be7ce152bd
NoahDammer <noah@thinksoft.ca>
2026-02-24 21:48:35 -0500

    feat: :tada: thinksoft_baron_labels inital commit
    #319

---
+++ thinksoft_baron_labels/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Thinksoft Baron Labels",
+    "description": """
+        Custom Labels for Baron Oilfield Supply
+    """,
+    "author": "Thinksoft Inc.",
+    "website": "http://www.thinksoft.ca",
+    "license": "LGPL-3",
+    "category": "Addons Custom/Thinksoft Baron Labels",
+    "version": "19.0.0.0",
+    "depends": ["base"],
+    "data": [
+        "reports/baron_1_25x4_templates.xml",
+        "reports/baron_2x4_templates.xml",
+    ],
+}
