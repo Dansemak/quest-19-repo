519fd1d652874fc55881246a632b31de8cc3f8c3
Dansemak Igga <dansemak@hotmail.com>
2026-02-25 21:13:17 +0000

    Baron labels (1.25x4 and 2x4 ) added

--- thinksoft_baron_labels/__manifest__.py
+++ thinksoft_baron_labels/__manifest__.py
@@ -8,9 +8,11 @@
     "license": "LGPL-3",
     "category": "Addons Custom/Thinksoft Baron Labels",
     "version": "19.0.0.0",
-    "depends": ["base"],
+    "depends": ["base", "mrp", "product"],
     "data": [
+        "reports/reports_paperformats.xml",
         "reports/baron_1_25x4_templates.xml",
         "reports/baron_2x4_templates.xml",
+        "reports/ir_actions_report.xml",
     ],
 }
