a2f695ed592d2413bb503392daf39566cbc5c364
NoahDammer <noah@thinksoft.ca>
2026-03-03 16:49:00 -0500

    refactor: :memo: Update category

--- auth_impersonate_user/__manifest__.py
+++ auth_impersonate_user/__manifest__.py
@@ -5,7 +5,7 @@
     """,
     "author": "Mint System GmbH",
     "website": "https://www.mint-system.ch",
-    "category": "Technical",
+    "category": "Addons Custom/Auth Impersonate User",
     "version": "19.0.1.0.0",
     "license": "AGPL-3",
     "depends": ["web"],
