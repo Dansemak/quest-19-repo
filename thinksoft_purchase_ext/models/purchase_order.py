e162e0e2a2efc42499ea0a11f6348c37a4d198bf
bigoa-gach <bigoagach@gmail.com>
2026-03-05 11:57:58 -0700

    feat: :heavy_plus_sign: added shipped date to purchase.order
    used this field for "Ship Date" on the purchase report instead of the epected arrival

--- thinksoft_purchase_ext/models/purchase_order.py
+++ thinksoft_purchase_ext/models/purchase_order.py
@@ -4,6 +4,11 @@ from odoo import fields, models
 class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
 
+    date_shipped = fields.Datetime(
+        string="Shipped Date",
+        help="The date in which the vendor must ship the products by.",
+    )
+
     carrier_id = fields.Many2one(
         "delivery.carrier",
         string="Carrier",
