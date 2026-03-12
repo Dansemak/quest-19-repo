471645a2c3aa5f238b9d297eb3bc3a4674b8cf94
Raoul Feze <jraoul.feze@gmail.com>
2026-03-12 14:37:27 -0600

    Formatted code

--- thinksoft_sale_ext/models/sale_order_line.py
+++ thinksoft_sale_ext/models/sale_order_line.py
@@ -2,12 +2,19 @@ from odoo import api, fields, models
 
 
 class SaleOrderLine(models.Model):
-    _inherit = 'sale.order.line'
-    _order = 'seq_no, id'
+    _inherit = "sale.order.line"
+    _order = "seq_no, id"
 
-    seq_no = fields.Integer(string="#", compute="_compute_line_number", readonly=True, store=True)
-    tagging = fields.Char(help="Customer Line Item Reference for custom identification or referencing of product in accordance to the customer")
-    string_availability_info = fields.Char(string="Availability", help="Estimated time when the product will be available from now (i.g 3-5 BUSINESS DAYS, 2 WEEKS, etc.)")
+    seq_no = fields.Integer(
+        string="#", compute="_compute_line_number", readonly=True, store=True
+    )
+    tagging = fields.Char(
+        help="Customer Line Item Reference for custom identification or referencing of product in accordance to the customer"
+    )
+    string_availability_info = fields.Char(
+        string="Availability",
+        help="Estimated time when the product will be available from now (i.g 3-5 BUSINESS DAYS, 2 WEEKS, etc.)",
+    )
     purchase_order_id = fields.Many2one("purchase.order", string="PO Number")
     weight = fields.Char(compute="_compute_weight")
     total_weight = fields.Char(compute="_compute_weight")
@@ -18,10 +25,15 @@ class SaleOrderLine(models.Model):
         for line in self:
             if line.order_id:
                 # getting all of the lines in the order
-                lines = line.order_id.order_line.filtered(lambda i: not i.display_type).sorted(key=lambda l: (l.sequence, l.id))
+                lines = line.order_id.order_line.filtered(
+                    lambda i: not i.display_type
+                ).sorted(key=lambda l: (l.sequence, l.id))
 
                 # finding the position of this line and assigning its line number
-                for i, o_l, in enumerate(lines, start=1):
+                for (
+                    i,
+                    o_l,
+                ) in enumerate(lines, start=1):
                     if o_l == line:
                         line.seq_no = i * 10
                         break
@@ -41,13 +53,23 @@ class SaleOrderLine(models.Model):
     def create(self, vals_list):
         if isinstance(vals_list, list):
             for vals in vals_list:
-                if "string_availability_info" in vals and vals["string_availability_info"]:
-                    vals["string_availability_info"] = vals["string_availability_info"].upper().strip()
+                if (
+                    "string_availability_info" in vals
+                    and vals["string_availability_info"]
+                ):
+                    vals["string_availability_info"] = (
+                        vals["string_availability_info"].upper().strip()
+                    )
                 if "tagging" in vals and vals["tagging"]:
                     vals["tagging"] = vals["tagging"].strip()
         else:
-            if "string_availability_info" in vals_list and vals_list["string_availability_info"]:
-                vals_list["string_availability_info"] = vals_list["string_availability_info"].upper().strip()
+            if (
+                "string_availability_info" in vals_list
+                and vals_list["string_availability_info"]
+            ):
+                vals_list["string_availability_info"] = (
+                    vals_list["string_availability_info"].upper().strip()
+                )
             if "tagging" in vals_list and vals_list["tagging"]:
                 vals_list["tagging"] = vals_list["tagging"].strip()
 
@@ -58,7 +80,9 @@ class SaleOrderLine(models.Model):
     def _onchange_string_availability_info(self):
         for record in self:
             if record.string_availability_info:
-                record.string_availability_info = record.string_availability_info.upper().strip()
+                record.string_availability_info = (
+                    record.string_availability_info.upper().strip()
+                )
 
     # trimming tagging whitespace on ends
     @api.onchange("tagging")
