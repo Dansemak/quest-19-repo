d3f211db6a8d3e6d2ee116a880bb197e50fbfb32
Raoul Feze <jraoul.feze@gmail.com>
2026-03-12 13:46:46 -0600

    Updated the version of thinksoft_sale_ext

--- thinksoft_sale_ext/__manifest__.py
+++ thinksoft_sale_ext/__manifest__.py
@@ -8,7 +8,7 @@
     "website": "http://www.thinksoft.ca",
     "license": "AGPL-3",
     "category": "Addons Custom/Thinksoft",
-    "version": "19.0.1.6",
+    "version": "19.0.1.7",
     "depends": [
         "base",
         "sale",
