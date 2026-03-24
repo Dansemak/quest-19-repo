a1f1e7b675381e05f154b61a1846ed37e2187c6c
NoahDammer <noah@thinksoft.ca>
2026-03-24 17:23:29 -0400

    feat: :tada: thinksoft_no_superuser
    #384

---
+++ thinksoft_no_superuser/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Thinksoft No Superuser",
+    "description": """
+        Remove 'Log in as superuser' on log in page.
+    """,
+    "author": "Thinksoft Inc.",
+    "website": "http://www.thinksoft.ca",
+    "license": "LGPL-3",
+    "category": "Addons Custom/Thinksoft",
+    "version": "19.0.1.0",
+    "depends": ["web"],
+    "data": [
+        "views/webclient_templates.xml",
+    ],
+}
