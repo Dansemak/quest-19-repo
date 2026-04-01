de96f10cd618fe574a55ad1967caf38068170496
NoahDammer <noah@thinksoft.ca>
2026-03-31 23:34:59 -0400

    feat: :bookmark: thinksoft_reports 19.0.1.17
    #387

--- thinksoft_reports/__manifest__.py
+++ thinksoft_reports/__manifest__.py
@@ -8,7 +8,7 @@
     "website": "http://www.thinksoft.ca",
     "license": "AGPL-3",
     "category": "Addons Custom/Thinksoft",
-    "version": "19.0.1.16",
+    "version": "19.0.1.17",
     "depends": ["base", "account"],
     "data": [
         "reports/reports_paperformats.xml",
